56e7682cf6d0a480e6185e297bfff20822528ecb
Kurtis <kurtie@microsoft.com>
2017-08-04 16:41:48 -0700

    Adding new loader to support UWP controller renderable API

---
+++ Assets/GLTF/Scripts/GLTFByteArrayLoader.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace GLTF
+{
+	public class GLTFByteArrayLoader : GLTFLoader
+	{
+		protected byte[] _gltfData;
+
+		public GLTFByteArrayLoader(byte[] gltfData, Transform parent = null) : base(null, parent)
+		{
+			_gltfData = gltfData;
+		}
+
+		public override IEnumerator Load(int sceneIndex = -1)
+		{
+			if (Multithreaded)
+			{
+				yield return asyncAction.RunOnWorkerThread(() => ParseGLTF(_gltfData));
+			}
+			else
+			{
+				ParseGLTF(_gltfData);
+			}
+
+			Scene scene;
+			if (sceneIndex >= 0 && sceneIndex < _root.Scenes.Count)
+			{
+				scene = _root.Scenes[sceneIndex];
+			}
+			else
+			{
+				scene = _root.GetDefaultScene();
+			}
+
+			if (scene == null)
+			{
+				throw new Exception("No default scene in gltf file.");
+			}
+
+			if (_lastLoadedScene == null)
+			{
+				if (_root.Buffers != null)
+				{
+					foreach (var buffer in _root.Buffers)
+					{
+						yield return LoadBuffer(buffer);
+					}
+				}
+
+				if (_root.Images != null)
+				{
+					foreach (Image image in _root.Images)
+					{
+						yield return LoadImage(image);
+					}
+				}
+
+				// generate these in advance instead of as-needed
+				if (Multithreaded)
+				{
+					yield return asyncAction.RunOnWorkerThread(BuildMeshAttributes);
+				}
+			}
+
+			GameObject sceneObj = CreateScene(scene);
+
+			if (_sceneParent != null)
+			{
+				sceneObj.transform.SetParent(_sceneParent, false);
+			}
+
+			_lastLoadedScene = sceneObj;
+		}
+	}
+}
