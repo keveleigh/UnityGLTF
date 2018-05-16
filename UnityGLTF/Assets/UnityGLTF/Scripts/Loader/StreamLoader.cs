1cd688179299f336140439ad03da7b38b00ee728
Kurtis Eveleigh <kurtie@microsoft.com>
2018-05-16 14:30:27 -0700

    Adding StreamLoader component

---
+++ UnityGLTF/Assets/UnityGLTF/Scripts/Loader/StreamLoader.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace UnityGLTF.Loader
+{
+    public class StreamLoader : ILoader
+    {
+        public Stream LoadedStream { get; private set; }
+
+        public StreamLoader(Stream loadedStream)
+        {
+            LoadedStream = loadedStream;
+        }
+
+        public IEnumerator LoadBaseStream()
+        {
+            if (LoadedStream == null)
+            {
+                throw new InvalidOperationException("LoadedStream is null.");
+            }
+
+            yield return null;
+        }
+
+        public IEnumerator LoadStream(string gltfFilePath)
+        {
+            throw new InvalidOperationException("StreamLoader does not load files.");
+        }
+    }
+}
